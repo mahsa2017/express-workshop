50fba99701722bd0c4acb1a2ab5b3959bb763e61
mahsa2017 <upmahjour@gmail.com>
2018-03-24 15:18:00 +0000

    step 7

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const formidable = require("express-formidable");
 app.listen(3000, () =>
     console.log("Server is listening on port 3000. Ready to accept requests!"));
 
@@ -11,6 +12,30 @@ app.listen(3000, () =>
 // app.get("/", function(req, res) {
 // console.log(req);
 // });
-app.get("/", function(req, res) {
-    res.send("Yay Node Girls!");
-  });
+// app.get("/", function(req, res) {
+//     res.send("Yay Node Girls!");
+//   });
+// app.get("/", function(req, res) {
+//     res.send("Hello World!");
+//   });
+  
+//   app.get("/chocolate", function(req, res) {
+//     res.send("Mm chocolate :O");
+//   });
+//   app.get("/node", function(req, res) {
+//     res.send("node is funny");
+//   });
+//   app.get("/girls", function(req, res) {
+//     res.send("girls are pretty");
+//   });
+app.use(express.static("public"));
+
+//app.use(formidable());
+
+app.post("/create-post", function(req, res) {
+  console.log('I am /create-post endpoint');
+  
+  console.log(req.fields);
+  console.log(req.body);
+});
+app.use(formidable());
