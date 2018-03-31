3d74efadec71ca2e5cdd92832b5ff5c99b1e0fb3
mahsa2017 <upmahjour@gmail.com>
2018-03-31 21:02:37 +0100

    shows posts but temperory

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const formidable = require("express-formidable");
 const fs = require("fs");
+
+
 app.listen(3000, () =>
   console.log("Server is listening on port 3000. Ready to accept requests!"));
 
@@ -29,21 +31,29 @@ app.listen(3000, () =>
 //   app.get("/girls", function(req, res) {
 //     res.send("girls are pretty");
 //   });
-app.use(express.static("public"));
+ app.use(express.static("public"));
 
-app.use(formidable());
+ app.use(formidable());
 
-app.post("/create-post", function (req, res) {
+ app.post("/create-post", function (req, res) {
   // console.log('I am /create-post endpoint');
-  console.log(req.fields);
+ console.log(req.fields)// contains non-file fields 
+ res.send(req.fields);
 });
 
-fs.writeFile("./data/posts_1.json", "Hello World", function(error) {
-  if (error) throw error;
-  console.log('The file has been saved!');
-});
+// fs.writeFile("./data/posts_1.json","Hello world", function(error) {
+//   //if (error) throw error;
+//   console.log('The file has been saved!');
+// });
+  
+// fs.readFile(__dirname + "/data/posts.json", function(error, file) {
+//   console.log(file.toString());
+//   const parsedFile = JSON.parse(file);
+// });
 
-fs.readFile(__dirname + "/data/posts.json", function(error, file) {
-  console.log(file.toString());
-  const parsedFile = JSON.parse(file);
-});
+// //how to read from a file and write to a new or another existing file
+// fs.readFile("readme.txt",'utf8',function(err,data){
+//    fs.writeFile('writeme.txt',data,function(err){
+//    });
+//   console.log(data,"for test");
+// });
