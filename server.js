9c4ac850b435815940e3207c78c3d1ab515dec5a
mahsa2017 <upmahjour@gmail.com>
2018-04-10 11:37:50 +0100

    port issue solved

--- server.js
+++ server.js
@@ -2,75 +2,48 @@ const express = require("express");
 const app = express();
 const formidable = require("express-formidable");
 const fs = require("fs");
+const serverport= process.env.PORT || 3000;
 
-
-app.listen(3000, () =>
-  console.log("Server is listening on port 3000. Ready to accept requests!"));
-
+app.listen(serverport, () =>
+  console.log(`Server is listening on port ${serverport}. Ready to accept requests!`));
 // req is the Request object, res is the Response object
 // (these are just variable names, they can be anything but it's a convention to call them req and res)
 // app.get("/", function (req, res) {
 //     res.send("Hello World!");
 // });
-
-// app.get("/", function(req, res) {
-//     res.send("Yay Node Girls!");
-//   });
-// app.get("/", function(req, res) {
-//     res.send("Hello World!");
-//   });
-
-//   app.get("/chocolate", function(req, res) {
-//     res.send("Mm chocolate :O");
-//   });
-//   app.get("/node", function(req, res) {
-//     res.send("node is funny");
-//   });
-//   app.get("/girls", function(req, res) {
-//     res.send("girls are pretty");
-//   });
  app.use(express.static("public"));
-
  app.use(formidable());
- var blogposts
- 
+ var blogposts;
  //fs.appendFile("./data/posts.json", parsedFile, function(error) {
   //if (error) throw error;
  // console.log('The file has been saved!');
 //});
 
-
-// fs.appendFile("./data/posts.json", parsedFile, function(error) {
-//   //if (error) throw error;
-//   console.log('The file has been saved!');
-// });
+//show/get the recent posts
 fs.readFile(__dirname + "/data/posts.json", function(error, file) {
-  console.log(file.toString());
-  blogposts = JSON.parse(file); //array
-  
+  //console.log(file.toString());
+  blogposts = JSON.parse(file); //json convert to array so then we can add and treat like an array rather than concat/append
 });
 app.get("/get-posts", function(req, res) {
   res.send(blogposts);
-console.log(req);
+  //console.log(req);
 });
-
+//create/send new post
 app.post("/create-post", function (req, res) {
   // console.log('I am /create-post endpoint');
- console.log(req.fields);// contains non-file fields 
+  //console.log(req.fields);// contains non-file fields 
  
 fs.readFile(__dirname + "/data/posts.json", function(error, file) {
-  console.log(file.toString());
+  //console.log(file.toString());
   blogposts = JSON.parse(file); //array
   const blogpost = req.fields;
-  console.log(blogposts,"blogpostssssssss")
+  //console.log(blogposts,"blogpostssssssss")
   blogposts.push(blogpost);
   fs.writeFile(__dirname + "/data/posts.json",JSON.stringify(blogposts),function(err){
-
       });
 });
 res.send(req.fields);
 });
-
 
 // //how to read from a file and write to a new or another existing file
 // fs.readFile("readme.txt",'utf8',function(err,data){
